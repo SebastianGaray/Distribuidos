016f3f2a62946da3349bf6f3bdde4e6c8d80b2d1
GarsePersen <sebastian.garay.p@usach.cl>
2020-01-21 19:42:49 -0300

    Se crea archivo de prueba de postgres

---
+++ ConsumerPSTest.py
@@ -0,0 +1,56 @@
+from kafka import KafkaConsumer
+import json
+import psycopg2 #Postgres
+from datetime import datetime
+from time import time
+
+###########CONEXION A POSTGRES######################################################################################### 
+try:
+    #connection = psycopg2.connect(user = "postgres", password="", host = "167.172.150.191", database = "distribuidos") #Remoto
+    connection = psycopg2.connect(user = "postgres", password="", host = "127.0.0.1", database = "distribuidos") #Local
+
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    #print ( connection.get_dsn_parameters(),"\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    #print("You are connected to - ", record,"\n")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+    
+#Se crea la tabal
+sql1 = "drop table if exists mensajes;"
+sql2 = "create table mensajes (id serial PRIMARY KEY,texto text, fecha date);"
+
+cursor.execute(sql1)
+cursor.execute(sql2)
+connection.commit()
+
+def start():
+    consumer = KafkaConsumer(
+        'test',
+        #bootstrap_servers=['167.172.150.191:9092'],
+        bootstrap_servers=['127.0.0.1:9092'],
+        auto_offset_reset='earliest',
+        group_id='group',
+        enable_auto_commit=True,
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+    print("Esperando mensajes...")
+    e = 0
+    startTime = time()
+    query_insert = "insert into mensajes(id,fecha) values (%s,%s)"
+    ids = 1
+    for message in consumer:
+        fecha = datetime.now()
+        message = message.value
+        
+        datos = (ids,fecha)
+        cursor.execute(query_insert,datos)
+        print("Consumidor: Recibiendo mensaje N: ",message)
+        
+        print("Mensajes por segundo: ", e/(time()-startTime))
+        e = e+1
+        ids+=1
---
+++ MainDBTest.py
@@ -0,0 +1,21 @@
+import ConsumerPSTest
+import ConsumerIgnite
+import ConsumerRedis
+import threading
+
+def startConsumerPS():
+    ConsumerPSTest.start()
+def startConsumerIgnite():
+    ConsumerIgnite.start()
+def startConsumerRedis():
+    ConsumerRedis.start()
+
+option = int(input("Ingrese opcion de BD(1: Comsumir en PS, 2: Consumir en Ignite, 3: Consumir en Redis): "))
+if(option == 1):
+    startConsumerPS()
+    
+if(option == 2):
+    startConsumerIgnite()
+    
+else:
+    startConsumerRedis()
